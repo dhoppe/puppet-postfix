3612cdcb8cd4a2be68a7cb73055ad0219028e0a6
Dennis Hoppe <dennis.hoppe@debian-solutions.de>
2012-02-01 03:25:44 +0100

    Initial commit

---
+++ Modulefile
@@ -0,0 +1,9 @@
+name 'dhoppe-postfix'
+version '0.0.1'
+
+author 'Dennis Hoppe'
+license 'GPLv2'
+project_page 'http://github.com/dhoppe/puppet-postfix'
+source 'git://github.com/dhoppe/puppet-postfix'
+summary 'Postfix Module'
+description 'Postfix Module for Debian, Ubuntu'
---
+++ manifests/init.pp
@@ -0,0 +1,85 @@
+class postfix {
+	define postfix::aliases ($email = false) {
+		$t_email = $email ? {
+			false   => "root",
+			default => $email,
+		}
+
+		file { "$name":
+			owner   => root,
+			group   => root,
+			mode    => 0644,
+			alias   => "aliases",
+			content => template("postfix/common/etc/aliases.erb"),
+			notify  => Exec["newaliases"],
+			require => Package["postfix"],
+		}
+	}
+
+	define postfix::relayhost ($host = false) {
+		$t_host = $host ? {
+			false   => "",
+			default => $host,
+		}
+
+		file { "$name":
+			owner   => root,
+			group   => root,
+			mode    => 0644,
+			alias   => "main.cf",
+			content => template("postfix/common/etc/postfix/main.cf.erb"),
+			notify  => Service["postfix"],
+			require => Package["postfix"],
+		}
+	}
+
+	exec { "newaliases":
+		command     => "newaliases",
+		refreshonly => true,
+	}
+
+	postfix::aliases { "/etc/aliases":
+		email => "admin@${::domain}",
+	}
+
+	file { "/etc/mailname":
+		owner   => root,
+		group   => root,
+		mode    => 0644,
+		alias   => "mailname",
+		content => "${::fqdn}\n",
+		notify  => Service["postfix"],
+		require => Package["postfix"],
+	}
+
+	postfix::relayhost { "/etc/postfix/main.cf":
+		host => "mail.${::domain}",
+	}
+
+	package { [
+		"exim4",
+		"exim4-base",
+		"exim4-config",
+		"exim4-daemon-light" ]:
+		ensure => absent,	
+	}
+
+	package { "postfix":
+		ensure => present,
+	}
+
+	service { "postfix":
+		enable     => true,
+		ensure     => running,
+		hasrestart => true,
+		hasstatus  => false,
+		require    => [
+			File["aliases"],
+			File["mailname"],
+			File["main.cf"],
+			Package["postfix"]
+		],
+	}
+}
+
+# vim: tabstop=3
