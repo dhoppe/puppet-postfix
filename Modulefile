b573c0888323ecf800fbc1a6ebe64aa524c100ec
Dennis Hoppe <dennis.hoppe@debian-solutions.de>
2012-04-11 13:57:45 +0200

    Fix several issues detected by puppet-lint

--- Modulefile
+++ Modulefile
@@ -1,5 +1,5 @@
 name 'dhoppe-postfix'
-version '0.0.5'
+version '0.0.6'
 
 author 'Dennis Hoppe'
 license 'GPLv2'
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,85 +1,85 @@
 class postfix {
 	define postfix::aliases($email = false) {
 		$t_email = $email ? {
-			false   => "root",
+			false   => 'root',
 			default => $email,
 		}
 
 		file { "$name":
 			owner   => root,
 			group   => root,
-			mode    => 0644,
-			alias   => "aliases",
-			content => template("postfix/common/etc/aliases.erb"),
-			notify  => Exec["newaliases"],
-			require => Package["postfix"],
+			mode    => '0644',
+			alias   => 'aliases',
+			content => template('postfix/common/etc/aliases.erb'),
+			notify  => Exec['newaliases'],
+			require => Package['postfix'],
 		}
 	}
 
 	define postfix::relayhost($host = false) {
 		$t_host = $host ? {
-			false   => "",
+			false   => '',
 			default => $host,
 		}
 
 		file { "$name":
 			owner   => root,
 			group   => root,
-			mode    => 0644,
-			alias   => "main.cf",
-			content => template("postfix/common/etc/postfix/main.cf.erb"),
-			notify  => Service["postfix"],
-			require => Package["postfix"],
+			mode    => '0644',
+			alias   => 'main.cf',
+			content => template('postfix/common/etc/postfix/main.cf.erb'),
+			notify  => Service['postfix'],
+			require => Package['postfix'],
 		}
 	}
 
-	exec { "newaliases":
-		command     => "newaliases",
+	exec { 'newaliases':
+		command     => 'newaliases',
 		refreshonly => true,
 	}
 
-	postfix::aliases { "/etc/aliases":
+	postfix::aliases { '/etc/aliases':
 		email => hiera('email'),
 	}
 
-	file { "/etc/mailname":
+	file { '/etc/mailname':
 		owner   => root,
 		group   => root,
-		mode    => 0644,
-		alias   => "mailname",
+		mode    => '0644',
+		alias   => 'mailname',
 		content => "${::fqdn}\n",
-		notify  => Service["postfix"],
-		require => Package["postfix"],
+		notify  => Service['postfix'],
+		require => Package['postfix'],
 	}
 
-	postfix::relayhost { "/etc/postfix/main.cf":
+	postfix::relayhost { '/etc/postfix/main.cf':
 		host => hiera('host'),
 	}
 
 	package { [
-		"exim4",
-		"exim4-base",
-		"exim4-config",
-		"exim4-daemon-light" ]:
-		ensure => absent,	
+		'exim4',
+		'exim4-base',
+		'exim4-config',
+		'exim4-daemon-light' ]:
+		ensure => absent,
 	}
 
 	package { [
-		"postfix",
-		"swaks" ]:
+		'postfix',
+		'swaks' ]:
 		ensure => present,
 	}
 
-	service { "postfix":
-		enable     => true,
+	service { 'postfix':
 		ensure     => running,
+		enable     => true,
 		hasrestart => true,
 		hasstatus  => false,
 		require    => [
-			File["aliases"],
-			File["mailname"],
-			File["main.cf"],
-			Package["postfix"]
+			File['aliases'],
+			File['mailname'],
+			File['main.cf'],
+			Package['postfix']
 		],
 	}
 }
